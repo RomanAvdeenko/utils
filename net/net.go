d43e4ceb0425f999a92d697f2d732f510397e7aa
RomanAvdeenko <yes@hl.ua>
2019-11-21 22:15:12 +0200

    20191121-home

--- net/net.go
+++ net/net.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"encoding/binary"
+	"fmt"
 	"github.com/RomanAvdeenko/utils/slice"
 	"net"
 )
@@ -29,10 +30,6 @@ func ExcludeInterfaces(in []net.Interface, excludeInterfaceNames []string) []net
 }
 
 func Ip2int(ipAddr []byte) (uint32, error) {
-	//ip := net.ParseIP(string(ipAddr))
-	//fmt.Printf("%#v\n", len(ip))
-	//if ip == nil {
-	//	return 0, errors.New("wrong ipAddr format")
-	//}
+	fmt.Printf("%v, len:%v\n", ipAddr, len(ipAddr))
 	return binary.BigEndian.Uint32(ipAddr), nil
 }
