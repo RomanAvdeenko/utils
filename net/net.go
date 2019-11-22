95080b4967e761f98157cf2654499419b065edd6
RomanAvdeenko <yes@hl.ua>
2019-11-22 14:59:42 +0200

    20191122

--- net/net.go
+++ net/net.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"encoding/binary"
+	"fmt"
 	"github.com/RomanAvdeenko/utils/slice"
 	"net"
 )
@@ -29,16 +30,25 @@ func ExcludeInterfaces(in []net.Interface, excludeInterfaceNames []string) []net
 }
 
 func Ip2int(ipAddr []byte) uint32 {
-	//fmt.Printf("%v, len:%v\n", ipAddr, len(ipAddr))
-	if len(ipAddr) > 4 {
-		return binary.BigEndian.Uint32(ipAddr[len(ipAddr)-4:])
+	fmt.Printf("%v, len:%v\n", ipAddr, len(ipAddr))
+	if len(ipAddr) > net.IPv4len {
+		return binary.BigEndian.Uint32(ipAddr[len(ipAddr)-net.IPv4len:])
 	}
 	return binary.BigEndian.Uint32(ipAddr)
 }
 
 func Broadcast(ipNet net.IPNet) net.IP {
+	brd := make(net.IP, net.IPv4len)
 	n := len(ipNet.IP)
-	brd := make(net.IP, n)
+	//fmt.Println(n)
+
+	if n > net.IPv4len {
+		for i := 0; i < net.IPv4len; i++ {
+			fmt.Println(">4")
+			brd[i] = ipNet.IP[n-net.IPv4len+i] | ^ipNet.Mask[i]
+		}
+		return brd
+	}
 	for i := 0; i < n; i++ {
 		brd[i] = ipNet.IP[i] | ^ipNet.Mask[i]
 	}
